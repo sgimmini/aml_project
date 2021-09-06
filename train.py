8587f88ebc6b04523827c16ef453e0ad07f5158b
smn57 <Gimmini@stud.uni-heidelberg.de>
2021-09-06 20:30:50 +0200

    fixed train script

--- train.py
+++ train.py
@@ -12,23 +12,23 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 compose = transforms.Compose([transforms.Resize((224, 224)),
-                               transforms.RandomHorizontalFlip(),
                                transforms.ToTensor(),
                                normalize])
-
 
 # init datasets
 traindata = ImageFolder("COVIDNet_ImageFolder/train", transform=compose)
 testdata = ImageFolder("COVIDNet_ImageFolder/test", transform=compose)
 
 # init dataloader
-train_loader = DataLoader(traindata, batch_size=32)
+train_loader = DataLoader(traindata, batch_size=64, shuffle=True)
 test_loader = DataLoader(testdata)
 
 # init model and copy weights
 network = models.resnet50(pretrained=False)
 network.load_state_dict(torch.load("models/converted_vissl_swav_covid_e950.torch"), strict=False)
 network.fc = nn.Linear(2048, 3)
+network.fc.weight.data.normal_(mean=0.0, std=0.01)
+network.fc.bias.data.zero_()
 
 # for name, parameter in network.named_parameters():
 #     if any(layer in name for layer in ['fc']):
@@ -38,10 +38,11 @@ network.fc = nn.Linear(2048, 3)
 #         parameter.requires_grad = False
 
 network = network.cuda()
+network.train()
 print(network)
 
-criterion = nn.CrossEntropyLoss().cuda()
-optimizer = optim.Adam(network.parameters(), lr=0.0001)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(network.parameters(), lr=0.0002)
 
 for epoch in range(NUM_EPOCHS):
     sum_loss = 0
@@ -50,10 +51,10 @@ for epoch in range(NUM_EPOCHS):
     for data, label in tqdm(train_loader):
         data = data.cuda()
         label = label.cuda()
-        optimizer.zero_grad()
         out = network(data)
         loss = criterion(out, label)
         sum_loss += loss.item()
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         _, predicted = torch.max(out.data, 1)
