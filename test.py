c16c69b28609d2ad59c59bba611329a9de043622
smn57 <Gimmini@stud.uni-heidelberg.de>
2021-09-06 20:29:40 +0200

    fixed test script

--- test.py
+++ test.py
@@ -12,7 +12,6 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 compose = transforms.Compose([transforms.Resize((224, 224)),
-                               transforms.RandomHorizontalFlip(),
                                transforms.ToTensor(),
                                normalize])
 
@@ -22,12 +21,12 @@ traindata = ImageFolder("COVIDNet_ImageFolder/train", transform=compose)
 testdata = ImageFolder("COVIDNet_ImageFolder/test", transform=compose)
 
 # init dataloader
-train_loader = DataLoader(traindata, batch_size=32)
 test_loader = DataLoader(testdata)
 # init model and copy weights
 network = models.resnet50(pretrained=False)
 network.fc = nn.Linear(2048, 3)
 network.load_state_dict(torch.load("models/converted_vissl_swav_covid_e950_e0_fc_only.torch"))
+network.eval()
 network = network.cuda()
 
 total = 0
